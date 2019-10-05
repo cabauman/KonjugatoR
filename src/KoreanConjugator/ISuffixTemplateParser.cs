b6c754f768928fd6a8a496b87569567a1fc67671
cabauman <cab452005@gmail.com>
2019-10-05 13:57:58 +0900

    Add suffix template parser

---
+++ src/KoreanConjugator/ISuffixTemplateParser.cs
@@ -0,0 +1,15 @@
+﻿namespace KoreanConjugator
+{
+    /// <summary>
+    /// A contract for parsing a suffix template.
+    /// </summary>
+    public interface ISuffixTemplateParser
+    {
+        /// <summary>
+        /// Parses the suffix template text into a suffix template object.
+        /// </summary>
+        /// <param name="templateText">The template text.</param>
+        /// <returns>A suffix template object.</returns>
+        SuffixTemplate Parse(string templateText);
+    }
+}
---
+++ src/KoreanConjugator/SuffixTemplateParser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace KoreanConjugator
+{
+    /// <summary>
+    /// Represents a suffix template parser.
+    /// </summary>
+    public class SuffixTemplateParser : ISuffixTemplateParser
+    {
+        /// <inheritdoc/>
+        public SuffixTemplate Parse(string templateText)
+        {
+            // V/A + [ㄹ/을] 거
+
+            string wordClass = "(?<WordClass>A/V|A|V)";
+            string noBadchim = "(?<NoBadchim>[ㅂ|ㄹ|ㄴ|ㅁ|가-힣])";
+            string badchim = "(?<Badchim>[가-힣])";
+            string optionalNoBadchim = $"(?:{noBadchim}/)?";
+            string badchimNoBadchim = $"\\({optionalNoBadchim}{badchim}\\)";
+            string optionalBadchimNoBadchim = $"(?:{badchimNoBadchim})?";
+            string pattern = $"{wordClass} \\+ {optionalBadchimNoBadchim}";
+
+            var regex = new Regex(pattern);
+            var match = regex.Match(templateText);
+
+            if (!match.Success)
+            {
+                throw new ArgumentException("Suffix doesn't match the template format.");
+            }
+
+            var wordClassResult = match.Groups["WordClass"].Value;
+            var badchimlessConnector = match.Groups["NoBadchim"].Value;
+            var badchimConnector = match.Groups["Badchim"].Value;
+
+            GroupCollection collection = match.Groups;
+
+            // Note that group 0 is always the whole match
+            for (int i = 1; i < collection.Count; i++)
+            {
+                Group group = collection[i];
+                string name = regex.GroupNameFromNumber(i);
+                Console.WriteLine("{0}: {1} {2}", name, group.Success, group.Value);
+            }
+
+            var template = new BadchimDependentSuffixTemplate(templateText, wordClassResult, badchimConnector, badchimlessConnector);
+
+            return template;
+        }
+    }
+}
