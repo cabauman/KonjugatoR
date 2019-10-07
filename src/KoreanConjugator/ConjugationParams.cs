c67569a18f6e8090902c2b086186076fe794f03a
cabauman <cab452005@gmail.com>
2019-10-07 21:15:23 +0900

    Use setters instead of a readonly struct

--- src/KoreanConjugator/ConjugationParams.cs
+++ src/KoreanConjugator/ConjugationParams.cs
@@ -3,31 +3,31 @@
     /// <summary>
     /// Represents a set of params used to specify the desired conjugation form.
     /// </summary>
-    public readonly struct ConjugationParams
+    public struct ConjugationParams
     {
         /// <summary>
-        /// Gets the world class.
+        /// Gets or sets the world class.
         /// </summary>
-        public WordClass WordClass { get; }
+        public WordClass WordClass { get; set; }
 
         /// <summary>
-        /// Gets a value indicating whether the conjugation should use honorific form.
+        /// Gets or sets a value indicating whether the conjugation should use honorific form.
         /// </summary>
-        public bool Honorific { get; }
+        public bool Honorific { get; set; }
 
         /// <summary>
-        /// Gets the tense.
+        /// Gets or sets the tense.
         /// </summary>
-        public Tense Tense { get; }
+        public Tense Tense { get; set; }
 
         /// <summary>
-        /// Gets the formality.
+        /// Gets or sets the formality.
         /// </summary>
-        public Formality Formality { get; }
+        public Formality Formality { get; set; }
 
         /// <summary>
-        /// Gets the clause type.
+        /// Gets or sets the clause type.
         /// </summary>
-        public ClauseType ClauseType { get; }
+        public ClauseType ClauseType { get; set; }
     }
 }
