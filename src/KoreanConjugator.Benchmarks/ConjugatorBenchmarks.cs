9eab4c81aff66df28215922f967c4ade0820b1f9
cabauman <cab452005@gmail.com>
2022-01-01 14:37:03 +0900

    chore: add benchmark project

---
+++ src/KoreanConjugator.Benchmarks/ConjugatorBenchmarks.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace KoreanConjugator.Benchmarks;
+
+[MemoryDiagnoser]
+[SimpleJob(launchCount: 1, warmupCount: 1, targetCount: 100)]
+public class ConjugatorBenchmarks
+{
+    private static readonly int Iterations = 100;
+    private readonly Conjugator _conjugator = new (new SuffixTemplateParser());
+    private readonly string _stem = "기다리";
+    private ConjugationParams _conjugationParams = new ()
+    {
+        ClauseType = ClauseType.Declarative,
+        Formality = Formality.InformalLow,
+        Honorific = false,
+        Tense = Tense.Present,
+        WordClass = WordClass.Verb,
+    };
+
+    [Benchmark]
+    public ConjugationResult[] Conjugate()
+    {
+        var results = new ConjugationResult[Iterations];
+        for (int i = 0; i < Iterations; i++)
+        {
+            results[i] = _conjugator.Conjugate(_stem, _conjugationParams);
+        }
+        return results;
+    }
+}
