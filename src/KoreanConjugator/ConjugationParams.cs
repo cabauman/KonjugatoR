9b147feb5a06a7b7b94825b37986c1c6485970c9
cabauman <cab452005@gmail.com>
2022-10-18 16:20:22 +0900

    refactor: make ConjugationParams struct readonly

--- src/KoreanConjugator/ConjugationParams.cs
+++ src/KoreanConjugator/ConjugationParams.cs
@@ -3,30 +3,30 @@
 /// <summary>
 /// Represents a set of params used to specify the desired conjugation form.
 /// </summary>
-public struct ConjugationParams
+public readonly struct ConjugationParams
 {
     /// <summary>
     /// Gets or sets the world class.
     /// </summary>
-    public WordClass WordClass { get; set; }
+    public WordClass WordClass { get; init; }
 
     /// <summary>
     /// Gets or sets a value indicating whether the conjugation should use honorific form.
     /// </summary>
-    public bool Honorific { get; set; }
+    public bool Honorific { get; init; }
 
     /// <summary>
     /// Gets or sets the tense.
     /// </summary>
-    public Tense Tense { get; set; }
+    public Tense Tense { get; init; }
 
     /// <summary>
     /// Gets or sets the formality.
     /// </summary>
-    public Formality Formality { get; set; }
+    public Formality Formality { get; init; }
 
     /// <summary>
     /// Gets or sets the clause type.
     /// </summary>
-    public ClauseType ClauseType { get; set; }
+    public ClauseType ClauseType { get; init; }
 }
