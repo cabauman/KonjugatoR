e2852ccdf03308fb165cf2acd8f592003066873b
Colt Bauman <cab452005@hotmail.com>
2024-03-10 13:47:44 +0900

    dev: add console project for experimenting

---
+++ src/KoreanConjugator.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using KoreanConjugator;
+
+Console.WriteLine("Hello, World!");
+
+ConjugationParams conjugationParams = new()
+{
+    ClauseType = ClauseType.Declarative,
+    Formality = Formality.InformalLow,
+    Honorific = false,
+    Tense = Tense.Present,
+    WordClass = WordClass.Verb,
+};
+
+Conjugator conjugator = new(new SuffixTemplateParser());
+string stem = "기다리";
+var result = conjugator.MemoryTest(stem, conjugationParams);
+//Console.WriteLine(result.Value);
